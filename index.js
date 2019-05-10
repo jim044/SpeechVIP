83e8968b146e8d09636d338136ab26f5e7e93dc5
root <root@jim044.jim044>
2019-05-10 14:29:48 +0200

    Save before testing

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
-require('./lib/accueil.js')(app)
+require('./lib/speechToWord.js')(app)
 require('./lib/convertSong.js')(app)
 require('./lib/videotoaudio.js')(app)
 
