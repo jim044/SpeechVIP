4c928f225e3f2d10552b3a98a502f2dbeb0ba26a
root <root@jim044.jim044>
2019-06-19 12:11:03 +0200

    Commit 19.06.2019

--- GOOGLEAPPLICATIONCREDENTIALS.sh
+++ GOOGLEAPPLICATIONCREDENTIALS.sh
@@ -1,4 +1,2 @@
 export GOOGLE_APPLICATION_CREDENTIALS="/home/jim044/Documents/Projets/SpeechTalk/SpeechVIP/Made the VIP talk-a1d663d9c857.json" 
-
-
 mount -t vboxsf Claude_francois '/home/jim044/Documents/Partage'
