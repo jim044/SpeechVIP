d763382cecc92bcbce81be2bbc199b976892fc84
jim044 <jim044@debian>
2019-06-28 06:45:07 +0100

    Commit 28.06.2019

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 })); 
 
 app.use('/css',express.static(__dirname +'/css'));
+app.use('/video',express.static(__dirname +'/video'));
+app.use('/node_modules',express.static(__dirname +'/node_modules'));
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + "/html/accueil.html");
@@ -18,6 +20,7 @@ require('./lib/speechToWord.js')(app)
 require('./lib/convertSong.js')(app)
 require('./lib/videotoaudio.js')(app)
 require('./lib/mergeVideo.js')(app)
+require('./lib/accueil.js')(app)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
