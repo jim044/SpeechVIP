44bda0dd3e5200f0260793e42150d12d54e5a42b
jim044 <jim044@debian>
2019-07-29 00:17:39 +0100

    Update 29.07.2019

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -16,12 +17,16 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname + "/html/accueil.html");
 });
 
+app.get('/adminSpeechVIP', function (req, res) {
+	res.sendFile(__dirname + "/html/admin.html");
+});
 
 require('./lib/speechToWord.js')(app)
 require('./lib/convertSong.js')(app)
 require('./lib/videotoaudio.js')(app)
 require('./lib/mergeVideo.js')(app)
 require('./lib/accueil.js')(app)
+require('./lib/admin.js')(app)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
