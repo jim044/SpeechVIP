d6a091a6ebb5043e786720c5f7bafa9c21447fdb
jim044 <jim044@debian>
2019-08-11 20:17:08 +0100

    Update 11.08.2019 21:17

--- GOOGLEAPPLICATIONCREDENTIALS.sh
+++ GOOGLEAPPLICATIONCREDENTIALS.sh
@@ -1,2 +1,2 @@
 export GOOGLE_APPLICATION_CREDENTIALS="/home/jim044/Documents/Projets/SpeechVIP/Made the VIP talk-a1d663d9c857.json" 
-mount -t vboxsf Partage '/home/jim044/Documents/Partage'
+sudo mount -t vboxsf Partage '/home/jim044/Documents/Partage'
